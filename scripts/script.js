6b22b053039fad2221fcc0cd1a993c054bb86dbc
Jesse Harlan <gotsanity@gmail.com>
2023-04-10 11:59:28 -0700

    initial

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+console.log("Running my script");
+
+// make a piece of data that represents a person
+
+let name = "Jesse Harlan";
+let gender = "Male";
+let age = 40;
+let height = 6;
+let weight = 300;
+let phone = "5551212";
+let email = "jesse.harlan@centralia.edu";
+
+
+let person = {
+    name: "Jesse Harlan",
+    gender: "Male",
+    age: 40
+};
+
+console.log(person.name);
+
+
+// array
+let persons = [];
+
+// make a function that tells whether the person is an adult
