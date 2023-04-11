028f51b4af7088fc2392f596e99603c6b04a9dea
Jesse Harlan <gotsanity@gmail.com>
2023-04-11 11:52:19 -0700

    added sum function

--- scripts/script.js
+++ scripts/script.js
@@ -12,15 +12,68 @@ let email = "jesse.harlan@centralia.edu";
 
 
 let person = {
-    name: "Jesse Harlan",
+    fname: "Jesse",
+    lname: "Harlan",
+    fullName: function() {
+        return this.fname + " " + this.lname;
+    },
     gender: "Male",
-    age: 40
+    age: 40,
+    sayHello: function() {
+        console.log("I did a thing!");
+        return this.fullName() + " says hello!";
+    }
 };
 
-console.log(person.name);
+// console.log(person.fullName());
 
+// console.log(fname);
+// console.log(person.fname);
 
 // array
 let persons = [];
 
 // make a function that tells whether the person is an adult
+
+
+
+// function that takes two numbers and returns the lower
+let num1 = 10;
+let num2 = 7;
+
+console.log(Min(num1, num2));
+console.log(num1); //
+
+
+// function declaration
+function Min(a, b) {
+    if (a < b) {
+        return a;
+    }
+    
+    return b;
+}
+
+// sum all numbers from 0 to N
+// test case sum(5) = 15
+
+
+console.log(Sum(5));
+
+function Sum(end) {
+    // start by creating a bucket
+    let result = 0;
+
+    // do the thing ?
+    while (end > 0) {
+        result = result + end;
+        end--;
+    }
+
+    // output
+    return result;
+}
+
+
+
+// console.log(a); // out of scope
